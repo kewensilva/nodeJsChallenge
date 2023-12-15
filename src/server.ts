a4b12d4204d8ad2127e868ceb60b910e95448033
kewensilva <kewen.macedo@gmail.com>
2023-12-15 05:26:47 -0300

    feat: created file server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from 'cors';
+import routes from './routes';
+import 'dotenv/config';
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes)
+
+
+app.listen(3000);
+
