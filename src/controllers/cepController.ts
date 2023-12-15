14e2103832881fda766327ba06f70e16b6719a25
kewensilva <kewen.macedo@gmail.com>
2023-12-15 05:30:55 -0300

    feat: cep controller created and middleware

---
+++ src/controllers/cepController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import * as cepMiddleware from '../middleware/cep'
+
+import { validateCep } from "../utils/validator";
+
+export const searchCep = async (req: Request, res: Response) => {
+    const { cep } = req.body;
+    try {
+        if (!validateCep(cep)) {
+            return res.status(400).send('CEP foi Digitado incorretamente, precisa conter 8 caracteres numericos!');
+        }
+
+        const resultSearchCep = await cepMiddleware.searchCep(cep);
+        res.json(resultSearchCep);
+    } catch (error) {
+        res.json({ error })
+    }
+
+};
